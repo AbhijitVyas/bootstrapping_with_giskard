031bed8c7618eca04495ae7c3c4d157ceb35b8e4
srikanth <srikanthmsk635@gmail.com>
2024-02-08 16:48:52 +0100

    added rasa ipython script

---
+++ start_rasa.py
@@ -0,0 +1,44 @@
+import rasa
+import requests
+from threading import Thread
+import subprocess
+import os
+
+dir = os.getcwd()
+newdir = './binder/rasa'
+
+if dir != newdir:
+    try:
+        os.chdir(newdir)
+        print("Changed directory to:", newdir)
+    except FileNotFoundError:
+        print("Directory not found:", newdir)
+else:
+    print("Already in the desired directory:", newdir)
+
+def run_bash_command(command):
+    process = subprocess.run(command, shell=True, capture_output=True, text=True)
+    print("Output:", process.stdout())
+    if process.stderr:
+        print("Error:", process.stderr)
+    
+if __name__ == '__main__':
+    # Define your Bash command
+    bash_command = "rasa run --enable-api"
+    
+    # Start Flask app in a separate thread
+    bash_thread = Thread(target=run_bash_command, args=(bash_command,))
+    bash_thread.start()
+    
+# Define your Bash command
+# bash_command = "rasa run --enable-api"
+
+# Run the Bash command
+# result = subprocess.run(bash_command, shell=True, capture_output=True, text=True)
+
+# Print the output
+# print("Output:", result.stdout)
+
+# Print the error message if any
+# if result.stderr:
+    # print("Error:", result.stderr)

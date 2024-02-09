39f8f97facdbbbf14be5b428f6b007a506896606
srikanth <srikanthmsk635@gmail.com>
2024-02-09 09:07:00 +0100

    shuffled rasa,bootstrap directories

--- binder/rasa/start_rasa.py
+++ binder/rasa/start_rasa.py
@@ -6,13 +6,17 @@ try:
     import rasa
 except ImportError:
     print("Rasa package does not exist")
-    rasa_command = "rasa run --enable-api"
+    rasa_command = "pip install rasa"
     process = subprocess.run(rasa_command, shell=True, capture_output=True, text=True)
     import rasa
-    print("Rasa installed")
+    print("Rasa installed and Imported")
     
 def run_bash_command(command):
-    process = subprocess.run(command, shell=True, capture_output=True, text=True)
+    # process = subprocess.run(command, shell=True, capture_output=True, text=True)
+    print("Rasa command")
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    for line in iter(process.stdout.readline, ''):
+        print(line, end='')
     # print("Output:", process.stdout())
     # if process.stderr:
     #     print("Error:", process.stderr)
